6a436641accb8a35472fdb77469880d81c312e9c
chaitanyayugo <chaitanyayugo@gmail.com>
2026-02-13 11:25:05 +0530

    Delete Js directory

--- Js/Script.js
+++
@@ -1,7 +0,0 @@
-// Basic: enable smooth reveal for .reveal elements
-const io = new IntersectionObserver((entries)=>{
-  entries.forEach(e => {
-    if (e.isIntersecting) e.target.classList.add('visible');
-  });
-},{threshold: 0.12});
-document.querySelectorAll('.reveal').forEach(el => io.observe(el));
